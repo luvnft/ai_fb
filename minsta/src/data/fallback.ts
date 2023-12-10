9ce897d521da0cc205ee92322086ec9ba0171266
luv nft <info@blkluv.org>
2023-12-10 09:12:14 -0500

    updated text

--- minsta/src/data/fallback.ts
+++ minsta/src/data/fallback.ts
@@ -1,9 +1,9 @@
 
 export const MINSTA_META = {
-  description: "Share your best moments with Mintbase and Minsta!",
-  title: "Mint Moments on Mintbase",
+  description: "Create and sell your best AI Music on Near blockchain.",
+  title: "Mint AI Music on LUV NFT",
   twitter:
-    "Explore%20unforgettable%20moments%20at%20%2Mintbase%20%40Mintbase%20%40NEARProtocol%20%23BOS%20%23NEAR",
+   "Create%20and%20sell%20your%20best%20AI%20Music%20at%20%23LUVNFT%20%40luvnft%20%40NEARProtocol%20%23NEAR%20%23AIMUSIC%F0%9F%8E%B5%0aMint%20yours%20here%3A%20https%3A%2F%2Faimusic.luvnft.com",
   image: "./images/thumbnail.png",
 };
 
